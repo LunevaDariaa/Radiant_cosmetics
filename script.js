7a9a39a31193a9212a077b82be4f45c55ef31f74
Daria Luneva <145506754+LunevaDariaa@users.noreply.github.com>
2023-12-04 21:46:24 -0500

    push review
    added some JS code to push user's review from textarea to reviews field

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const sendBtn = document.querySelector(".submit");
 const modal = document.querySelector(".modal");
 const closeBtn = document.querySelector(".close");
 const overlay = document.querySelector(".overlay");
+const reviews = document.querySelector(".reviews");
+
 sendBtn.addEventListener("click", function (event) {
   event.preventDefault();
   //   console.log(feedback.value);
   let msg = feedback.value;
-  console.log(msg);
+
+  //Create and push new review into review field
+  const newReview = `<div class="review">
+  <p class="rating">⭐⭐⭐⭐⭐ (5 stars)</p>
+  <p class="feedback">
+    " ${msg}"
+  </p>
+  <p class="author">Lisa N.</p>
+  <button class="like-button">&heartsuit;</button>
+  <span class="like-count">1 like</span>
+</div>`;
+  reviews.insertAdjacentHTML("afterbegin", newReview);
+
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
   closeFN();
